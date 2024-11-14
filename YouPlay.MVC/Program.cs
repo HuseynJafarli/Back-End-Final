2dd2156159e59008a921abe6e30662e55d07f5df
Huseyn Jafarli <hjeferli18088@ada.edu.az>
2024-11-14 23:57:29 +0400

    Area added

--- YouPlay.MVC/Program.cs
+++ YouPlay.MVC/Program.cs
@@ -27,8 +27,13 @@ namespace YouPlay.MVC
             app.UseAuthorization();
 
             app.MapControllerRoute(
+               name: "areas",
+               pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+            app.MapControllerRoute(
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}");
+
 
             app.Run();
         }
--- YouPlay.MVC/obj/Debug/net8.0/YouPlay.MVC.AssemblyInfo.cs
+++ YouPlay.MVC/obj/Debug/net8.0/YouPlay.MVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("YouPlay.MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe77d5b4919de8dd44de5ac3ac5122e94faf8050")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06d583367c9cc092b78b13a02cde1df18d74207d")]
 [assembly: System.Reflection.AssemblyProductAttribute("YouPlay.MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("YouPlay.MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
